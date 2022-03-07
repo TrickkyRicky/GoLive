98fd731faf5934bd3b933d215ad6bd63abddc52c
trickkyricky <rickygyoungjr@gmail.com>
2022-03-07 15:08:58 -0600

    Changed Routing

---
+++ FrontEnd/src/pages/Auth/NewPass.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPass = (props) => {
+  return <div>This is the NewPass page</div>;
+};
+
+export default NewPass;
---
+++ FrontEnd/src/pages/Auth/Register.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = (props) => {
+  return <div>This is the Register page</div>;
+};
+
+export default Register;
