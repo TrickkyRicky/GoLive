36f77a0206fa217e181d8e38a96d8a2c5a146a79
trickkyricky <rickygyoungjr@gmail.com>
2022-03-07 17:42:17 -0600

    Frontend test | authentication complete

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,8 @@
 import Home from "./pages/Home";
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
-// import ForgotPass from "./pages/Auth/ForgotPass.jsx";
-// import NewPass from "./pages/Auth/NewPass.jsx";
+import ForgotPass from "./pages/Auth/ForgotPass.jsx";
+import NewPass from "./pages/Auth/NewPass.jsx";
 import Settings from "./pages/Settings";
 import Error404 from "./pages/Error404.jsx";
 import Upload from "./pages/Upload";
@@ -10,26 +10,25 @@ import Stream from "./pages/Stream";
 import Layout from "./components/Layout";
 import AuthLayout from "./components/AuthLayout";
 
-import {
-  Routes,
-  Route
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="auth" element={<AuthLayout />}>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+      <Route path="Auth" element={<AuthLayout />}>
+        <Route path="Login" element={<Login />} />
+        <Route path="Register" element={<Register />} />
+        {/* <Route path="ForgotPass" element={<ForgotPass />} />
+        <Route path="NewPass/:id" element={<NewPass />} /> */}
       </Route>
 
       <Route path="/" element={<Layout />}>
-        <Route path="home" element={<Home />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="upload" element={<Upload />} />
+        <Route path="Home" element={<Home />} />
+        <Route path="Settings" element={<Settings />} />
+        <Route path="Upload" element={<Upload />} />
 
         {/* after Stream will be Stream/username */}
-        <Route path="stream/:username" element={<Stream />} />
+        <Route path="Stream/:username" element={<Stream />} />
         <Route path="*" element={<Error404 />} />
       </Route>
     </Routes>
