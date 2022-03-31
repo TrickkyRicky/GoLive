7de0dd01d6466ec7f8a3ab5c85e6ca5e59cf735e
Michael Flores <michael.fx.098@gmail.com>
2022-03-31 18:02:33 -0400

    upload.

--- FrontEnd/src/pages/Upload.jsx
+++ FrontEnd/src/pages/Upload.jsx
@@ -16,7 +16,7 @@ import { uploadVideo } from "../store/user/user-actions";
 import { listCategories } from "../store/content/content-actions";
 import { contentActions } from "../store/content/content-slice";
 
-const Upload = (props) => {
+const Upload = () => {
   const dispatch = useDispatch();
 
   const auth = useSelector((state) => {
@@ -30,65 +30,70 @@ const Upload = (props) => {
   const isShown = useSelector((state) => state.content.showUploadModal);
 
   const [values, setValues] = useState({
-    title: "",
-    category: "",
-    isStreamed: "false",
-    video: "",
-    thumbnail: "",
+    title: '',
+    description: '',
+    category: '',
+    isStreamed: 'false',
+    video: '',
+    thumbnail: ''
   });
 
   const categoryNames = useSelector((state) => state.content.categoryNames);
 
   const handleChange = (e, field) => {
-    let value =
-      field == "video" || field == "thumbnail"
-        ? e.target.files[0]
-        : e.target.value;
+    let value = (field == 'video' || field == 'thumbnail') ? e.target.files[0] : e.target.value;
 
     setValues({
       ...values,
-      [field]: value,
-    });
-  };
+      [field]: value
+    })
+  }
 
   const clickSubmit = (e) => {
     e.preventDefault();
 
     let newVideo = new FormData();
 
-    values.title && newVideo.append("title", values.title);
-    values.category && newVideo.append("category", values.category);
-    values.isStreamed && newVideo.append("isStreamed", values.isStreamed);
-    values.video && newVideo.append("video", values.video);
-    values.thumbnail && newVideo.append("thumbnail", values.thumbnail);
+    values.title && newVideo.append('title', values.title);
+    values.description && newVideo.append('description', values.description);
+    values.category && newVideo.append('category', values.category);
+    values.isStreamed && newVideo.append('isStreamed', values.isStreamed);
+    values.video && newVideo.append('video', values.video);
+    values.thumbnail && newVideo.append('thumbnail', values.thumbnail);
 
-    console.log(props.jwt);
-    dispatch(uploadVideo(props.jwt, newVideo));
-  };
+    dispatch(uploadVideo(auth.jwt, newVideo));
+  }
 
   const hideModal = () => {
     dispatch(contentActions.showUploadModal(false));
-  };
+  }
 
   useEffect(() => {
     dispatch(listCategories());
   }, []);
 
   return (
-    <Container>
-      <Row>
-        <Col>
+    <Modal
+        show={isShown}
+        onHide={hideModal}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <h2 id="contained-modal-title-vcenter">
+            UPLOAD A VIDEO
+          </h2>
+        </Modal.Header>
+        <Modal.Body>
           <Form className="upload-form">
             <Form.Group controlId="formFile" className="mb-3">
               <Form.Label>Upload Video</Form.Label>
-              <Form.Control
-                type="file"
-                onChange={(e) => handleChange(e, "video")}
-              />
+              <Form.Control type="file" onChange={(e) => handleChange(e, 'video')}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicTitle">
-              <Form.Label>Title</Form.Label>
+              <Form.Label>Video Title</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter Video Title"
@@ -97,35 +102,41 @@ const Upload = (props) => {
               />
             </Form.Group>
 
-            <Form.Group controlId="formThumbnailFile" className="mb-3">
-              <Form.Label>Thumbnail</Form.Label>
+            <Form.Group className="mb-3" controlId="formDescription">
+              <Form.Label>Description</Form.Label>
               <Form.Control
-                type="file"
-                onChange={(e) => handleChange(e, "thumbnail")}
+                as="textarea"
+                placeholder="Enter Video Description"
+                onChange={(e) => handleChange(e, "description")}
+                value={values.description}
               />
+            </Form.Group>
+
+            <Form.Group controlId="formThumbnailFile" className="mb-3">
+              <Form.Label>Upload Thumbnail</Form.Label>
+              <Form.Control type="file" onChange={(e) => handleChange(e, 'thumbnail')}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicCategory">
               <Form.Label>Category</Form.Label>
-              <Form.Select
-                aria-label="Select Category"
-                onChange={(e) => handleChange(e, "category")}
-                value={values.category}
-              >
+              <Form.Select aria-label="Select Category"  onChange={(e) => handleChange(e, "category")} value={values.category}>
                 <option>Select Category</option>
-                {categoryNames.map((title) => {
-                  return <option value={title}>{title}</option>;
-                })}
+                {
+                  categoryNames.map((title, i) => {
+                    return (
+                      <option key={i} value={title}>{title}</option>
+                    )
+                  })
+                }
               </Form.Select>
             </Form.Group>
 
-            <Button variant="primary" type="submit" onClick={clickSubmit}>
+            <button type="submit" className="publish-btn" onClick={clickSubmit}>
               Publish
-            </Button>
+            </button>
           </Form>
-        </Col>
-      </Row>
-    </Container>
+        </Modal.Body>
+      </Modal>
   );
 };
 
